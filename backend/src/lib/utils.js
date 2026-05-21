d776d9663fe87241ab42bc3d1b26692744ec5246
Sanju Sajan <sanjusam278@gmail.com>
2026-05-21 11:18:54 +0530

    signup section code refractor

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+     const { JWT_SECRET, NODE_ENV } = process.env;
+        if(!JWT_TOKEN){
+            throw new Error("JWT_SECRET is not configured")
+        } 
+    const token = jwt.sign({ userId }, JWT_SECRET, {
         expiresIn: "7d",
 });
 res.cookie("jwt", token, {
