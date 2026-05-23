34492535ef4bc8495d2932ac1f475d66e663c362
Sanju Sajan <sanjusam278@gmail.com>
2026-05-23 16:07:25 +0530

    all features were applied

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import BorderAnimatedContainer from '../components/BorderAnimatedContainer'
-import { useChatStore } from '../store/useChatStore';
-import ProfileHeader from '../components/ProfileHeader';
-import ChatList from '../components/ChatList';
-import ContactsList from '../components/ContactsList';
-import ActiveTabSwitch from '../components/ActiveTabSwitch';
-import NoConversationPlaceHolder from '../components/NoConversationPlaceHolder';
-import ChatContainer from '../components/ChatContainer';
+import { useChatStore } from "../store/useChatStore";
+
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
+import ProfileHeader from "../components/ProfileHeader";
+import ActiveTabSwitch from "../components/ActiveTabSwitch";
+import ChatsList from "../components/ChatsList";
+import ContactList from "../components/ContactList";
+import ChatContainer from "../components/ChatContainer";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 
 function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
+
   return (
     <div className="relative w-full max-w-6xl h-[800px]">
-      
       <BorderAnimatedContainer>
-      <div className='w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col '>
-      <ProfileHeader />
-      <ActiveTabSwitch />
+        {/* LEFT SIDE */}
+        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
 
-      <div className="flex-1 overflow-y-auto p-4 space-y-2">
-        {activeTab === "chats" ? <ChatList /> : <ContactsList />}
-      </div>
-      </div>
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+          </div>
+        </div>
 
-      <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">{selectedUser ? <ChatContainer /> : <NoConversationPlaceHolder />}
-      </div>
-
+        {/* RIGHT SIDE */}
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+          {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+        </div>
       </BorderAnimatedContainer>
-
     </div>
-  )
+  );
 }
-
-export default ChatPage
+export default ChatPage;
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,18 +1,25 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
-export const useAuthStore = create((set) => ({
+import { io } from "socket.io-client";
+
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+
+export const useAuthStore = create((set, get) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
   isLoggingIn: false,
+  socket: null,
+  onlineUsers: [],
 
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
+      get().connectSocket();
     } catch (error) {
-      console.log("Error in authCheck", error);
+      console.log("Error in authCheck:", error);
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
@@ -26,12 +33,14 @@ export const useAuthStore = create((set) => ({
       set({ authUser: res.data });
 
       toast.success("Account created successfully!");
+      get().connectSocket();
     } catch (error) {
-          toast.error(error.response.data.message);
+      toast.error(error.response.data.message);
     } finally {
       set({ isSigningUp: false });
     }
   },
+
   login: async (data) => {
     set({ isLoggingIn: true });
     try {
@@ -39,30 +48,57 @@ export const useAuthStore = create((set) => ({
       set({ authUser: res.data });
 
       toast.success("Logged in successfully");
+
+      get().connectSocket();
     } catch (error) {
-          toast.error(error.response.data.message);
+      toast.error(error.response.data.message);
     } finally {
       set({ isLoggingIn: false });
     }
   },
+
   logout: async () => {
     try {
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
+      get().disconnectSocket();
     } catch (error) {
       toast.error("Error logging out");
-      console.log("Logout error", error);
+      console.log("Logout error:", error);
     }
   },
-  updateProfile: async (data) =>{
+
+  updateProfile: async (data) => {
     try {
       const res = await axiosInstance.put("/auth/update-profile", data);
       set({ authUser: res.data });
       toast.success("Profile updated successfully");
     } catch (error) {
+      console.log("Error in update profile:", error);
       toast.error(error.response.data.message);
-      console.log("Update profile error", error);
     }
-  }
+  },
+
+  connectSocket: () => {
+    const { authUser } = get();
+    if (!authUser || get().socket?.connected) return;
+
+    const socket = io(BASE_URL, {
+      withCredentials: true, // this ensures cookies are sent with the connection
+    });
+
+    socket.connect();
+
+    set({ socket });
+
+    // listen for online users event
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
+  },
+
+  disconnectSocket: () => {
+    if (get().socket?.connected) get().socket.disconnect();
+  },
 }));
