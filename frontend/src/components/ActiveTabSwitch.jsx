9fd17f3a1396c02c2e537f9d7584ff9b81e25026
Sanju Sajan <sanjusam278@gmail.com>
2026-05-23 13:26:14 +0530

    sidebar completed

--- frontend/src/components/ActiveTabSwitch.jsx
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useChatStore } from '../store/useChatStore'
 
 function ActiveTabSwitch() {
+  const { activeTab, setActiveTab } = useChatStore();
   return (
-    <div>ActiveTabSwitch</div>
+    <div className='tabs tabs-boxed bg-transparent p-2 m-2 '>
+      <button  className={`tab ${
+          activeTab === "chats" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"
+        }`} onClick={()=> setActiveTab("chats")}>Chats</button>
+      <button  className={`tab ${
+          activeTab === "contacts" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"
+        }`} onClick={()=> setActiveTab("contacts")}>Contacts</button>
+    </div>
   )
 }
 
